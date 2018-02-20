5d01ef06914551d6c79d7c61af862ed7adaba054
Jason Triche <trichejason@gmail.com>
2018-02-20 00:17:29 -0600

    rest api consumable

--- src/main/java/com/lyft/java/v1/ConsumeController.java
+++ src/main/java/com/lyft/java/v1/ConsumeController.java
@@ -8,18 +8,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import java.io.IOException;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 @Controller
 public class ConsumeController {
-    Quote quot1 = new Quote();
+    //Quote quot1 = new Quote();
 
     @RequestMapping("/consume")
     public String consume(Model model) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet("https://data.cityofchicago.org/resource/4wp6-czhu.json");
 
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        quot1 = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", quot1.class);
-        model.addAttribute("quote", quot1);
+            System.out.println("Executing request " + httpget.getRequestLine());
+
+            // Create a custom response handler
+            ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+                @Override
+                public String handleResponse(
+                        final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+
+            };
+            String responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            httpclient.close();
+        } catch(Exception e){
+            System.out.println("******************");
+            System.out.println(e);
+        }finally {
+           // httpclient.close();
+        }
+
         return "consume";
     }
 }
